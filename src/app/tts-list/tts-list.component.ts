a6627808dc877855cc1d5ed246ce145df8b1493f
RyanMontville <ryan.a.montville@gmail.com>
2024-07-29 10:27:56 -0400

    tts page now sets preferred voice OnInIT

--- src/app/tts-list/tts-list.component.ts
+++ src/app/tts-list/tts-list.component.ts
@@ -15,22 +15,40 @@ export class TtsListComponent implements OnInit {
   currentIndex: number = 0;
   checklistComplete: boolean = false;
   steps: string[] = ['Raise stabilizer jacks', 'Put pads in blue bucket', 'Put on weight distribution hitch', 'Raise trailer tongue', 'Back up to align', 'Lower on to pilot', 'Latch tongue with coupling pin', 'Criss cross attach chains', 'Manual crank Raise high for ease of bars', 'Loosen sway bolt with Allen wrench', 'Adjust wings to be in line with trailer', 'Attach bars and pins', 'Lower and retract tongue jack', 'Attach emergency brake cable to car', 'Verify emergency brake pin in place on trailer', 'Verify battery connected (red quick disconnect connection)', 'Attach 7 way connector - make sure it cannot drag', 'Stow drill, Allen wrench, WD lever, and pads', 'Remove and stow chocks', 'Checklist completed'];
-  currentItem: string = this.steps[this.currentIndex];
+  currentItem: string = this.steps[0];
   voices!: SpeechSynthesisVoice[];
   selectedVoice!: SpeechSynthesisVoice;
   deviceHasTTS: boolean = true;
   numberOfVoicesFound: number = 0;
-
+  preferrdVoices: string[] = ['English (United States)', 'Microsoft Zira - English (United States)', 'Moira'];
 
 
   constructor() {
     this.uttr = new SpeechSynthesisUtterance();
   }
   ngOnInit(): void {
+    this.getVoices();
+    if (!this.deviceHasTTS) {
+      setTimeout(() => {
+        window.location.reload();
+      }, 100);
+    }
+    let voiceFound = this.voices.find(voice => {
+      return this.preferrdVoices.includes(voice.name);
+    });
+    if (voiceFound) {
+      this.selectedVoice = voiceFound;
+    } else {
+      this.selectedVoice = this.voices[0];
+    }
+  }
+
+  getVoices() {
     this.voices = speechSynthesis.getVoices();
     this.numberOfVoicesFound = this.voices.length;
+    console.log(`first try to find voices, found ${this.numberOfVoicesFound}`);
     if (this.voices.length > 0) {
-      this.selectedVoice = this.voices[0];
+      this.deviceHasTTS = true;
     } else {
       this.deviceHasTTS = false;
     }
@@ -49,6 +67,7 @@ export class TtsListComponent implements OnInit {
       this.checklistComplete = true;
     } else {
       this.currentIndex += 1;
+      this.currentItem = this.steps[this.currentIndex];
     }
   }
 }
